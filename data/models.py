322c26c34b9f3d5223799d6a1bc5f95ea57836ef
unknown <erjuanfri01@gmail.com>
2024-07-08 19:08:55 +0200

    Añadir a los modelos de las beachcams los atributos para la url de youtube y del prefered image provider, y seedear como mas beachcams

--- data/models.py
+++ data/models.py
@@ -7,8 +7,11 @@ class BeachCam(models.Model):
     beach_latitude = models.DecimalField(max_digits=9, decimal_places=6)
     beach_longitude = models.DecimalField(max_digits=9, decimal_places=6)
     url_image = models.CharField(max_length=200, null=True)
+    url_seemallorca = models.CharField(max_length=200, null=True, blank=True)
     url_aemet = models.CharField(max_length=200, null=True, blank=True)
     url_platgesbalears = models.CharField(max_length=200, null=True, blank=True)
+    url_youtube = models.CharField(max_length=200, null=True, blank=True)
+    prefered_image_provider = models.CharField(max_length=25, null=False, default="youtube")
     probe_freq_mins = models.IntegerField(default=60)
     description = models.CharField(max_length=255, null=True)
 
