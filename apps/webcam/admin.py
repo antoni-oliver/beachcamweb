bf48091ab28261a4e6521fb35b7d63f8d128bc85
PBibiloni <pedro@bibiloni.es>
2025-04-10 23:59:58 +0200

    Merge providers into webcam model

--- apps/webcam/admin.py
+++ apps/webcam/admin.py
@@ -3,6 +3,3 @@ from django.contrib import admin
 from apps.webcam import models
 
 admin.site.register(models.WebCam)
-admin.site.register(models.ImageProvider)
-admin.site.register(models.StreamProvider)
-admin.site.register(models.YoutubeProvider)
--- apps/webcam/models.py
+++ apps/webcam/models.py
@@ -25,11 +25,16 @@ class WebCam(models.Model):
     num_consecutive_failures = models.IntegerField(default=0)
     probe_freq_mins = models.IntegerField(default=60)
     max_crowd_count = models.IntegerField(default=0)
-    # Cam provider
+    # Webcam info
     public_url = models.CharField(max_length=2048, blank=True, null=True, help_text="URL to redirect viewers to original source.")
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
-    provider = GenericForeignKey("content_type", "object_id")
+    video_seconds = models.IntegerField(default=10, help_text="Seconds to record for the video", blank=True)
+    # Webcam info: if provider is static image
+    provider_image_url = models.CharField(max_length=2048, help_text="Can use `%Y`, `%m`, `%d`, etc. as in python's strftime(...)", blank=True, null=True)
+    # Webcam info: if provider is m3u8 stream
+    provider_stream_url = models.CharField(max_length=2048, help_text="Use .m3u8 or .html that generates a .m3u8 after clicking on a html element.", blank=True, null=True)
+    provider_stream_clickable_element_xpath = models.CharField(max_length=2048, help_text="XPath to the clickable element that generates the stream.", blank=True, null=True)
+    # Webcam info: if provider is static image
+    provider_youtube_url = models.CharField(max_length=2048, help_text="Only for https://www.youtube.com/watch?v=(...) links.", blank=True, null=True)
 
     def __str__(self):
         return self.beach_name
@@ -56,7 +61,7 @@ class WebCam(models.Model):
         from apps.prediction.models import Snapshot
         ts = timezone.now()
         try:
-            video_path, image_path = self.provider.download_video_and_image(timestamp=ts)
+            video_path, image_path = self.download_video_and_image(timestamp=ts)
             snapshot = Snapshot.objects.create(
                 webcam=self,
                 ts=ts,
@@ -72,63 +77,28 @@ class WebCam(models.Model):
             self.num_consecutive_failures += 1
             self.save()
 
-
-class ImageProvider(models.Model):
-    webcam = GenericRelation(WebCam, related_query_name="image_provider")
-    url = models.CharField(max_length=2048, help_text="Can use `%Y`, `%m`, `%d`, etc. as in python's strftime(...)")
-
-    def __str__(self):
-        return f"ImageProvider({self.webcam.first()}): {self.url}"
-
     def download_video_and_image(self, timestamp=None):
+        """ Downloads the video and image from the provider. """
         timestamp = timestamp or timezone.now()
-        image_path = self.webcam.first().relative_filepath(timestamp=timestamp, subfolder='img/originals/', extension='.mp4')
-        url = timestamp.strftime(self.url)
-        with open(os.path.join(settings.MEDIA_ROOT, image_path), 'wb') as f:
-            f.write(requests.get(url, verify=False).content)
-        return None, image_path
-
-
-class StreamProvider(models.Model):
-    webcam = GenericRelation(WebCam, related_query_name="stream_provider")
-    url = models.CharField(max_length=2048, help_text="Use .m3u8 or .html that generates a .m3u8 after clicking on a html element.")
-    clickable_element_xpath = models.CharField(max_length=2048)
-    seconds = models.IntegerField(default=10, help_text="Seconds to record for the video", blank=True)
-
-    def __str__(self):
-        return f"StreamProvider({self.webcam.first()}): {self.url}"
-
-    def download_video_and_image(self, timestamp=None):
-        timestamp = timestamp or timezone.now()
-        webcam = self.webcam.first()
-        image_path = webcam.relative_filepath(timestamp=timestamp, subfolder='img/originals/', extension='.jpg')
-        video_path = webcam.relative_filepath(timestamp=timestamp, subfolder='vid/originals/', extension='.mp4')
-        stream_urls = utils.m3u8_from_url(self.url, self.clickable_element_xpath)
-        utils.video_and_image_from_m3u8(stream_urls[0], self.seconds, os.path.join(settings.MEDIA_ROOT, video_path), os.path.join(settings.MEDIA_ROOT, image_path))
-        return video_path, image_path
-
-
-class YoutubeProvider(models.Model):
-    webcam = GenericRelation(WebCam, related_query_name="stream_provider")
-    url = models.CharField(max_length=2048, help_text="Only for https://www.youtube.com/watch?v=(...) links.")
-    seconds = models.IntegerField(default=10, help_text="Seconds to record for the video", blank=True)
-
-    def __str__(self):
-        return f"YoutubeProvider({self.webcam.first()}): {self.url}"
-
-    def download_video_and_image(self, timestamp=None):
-        """ Provides the path to the stream, and the path to the image."""
-        timestamp = timestamp or timezone.now()
-        webcam = self.webcam.first()
-        with YoutubeDL({'format': 'bestvideo/best'}) as ydl:
-            result = ydl.extract_info(
-                self.url,
-                download=False  # We just want to extract the info
-            )
-        video = result['entries'][0] if 'entries' in result else result  # Can be a playlist or a list of videos
-        stream_url = video['url']
-
-        image_path = webcam.relative_filepath(timestamp=timestamp, subfolder='img/originals/', extension='.jpg')
-        video_path = webcam.relative_filepath(timestamp=timestamp, subfolder='vid/originals/', extension='.mp4')
-        utils.video_and_image_from_m3u8(stream_url, self.seconds, os.path.join(settings.MEDIA_ROOT, video_path), os.path.join(settings.MEDIA_ROOT, image_path))
-        return video_path, image_path
+        image_path = self.relative_filepath(timestamp=timestamp, subfolder='img/originals/', extension='.jpg')
+        video_path = self.relative_filepath(timestamp=timestamp, subfolder='vid/originals/', extension='.mp4')
+        if self.provider_image_url:
+            url = timestamp.strftime(self.provider_image_url)
+            with open(os.path.join(settings.MEDIA_ROOT, image_path), 'wb') as f:
+                f.write(requests.get(url, verify=False).content)
+            return None, image_path
+        elif self.provider_stream_url:
+            timestamp = timestamp or timezone.now()
+            stream_urls = utils.m3u8_from_url(self.provider_stream_url, self.provider_stream_clickable_element_xpath)
+            utils.video_and_image_from_m3u8(stream_urls[0], self.video_seconds, os.path.join(settings.MEDIA_ROOT, video_path), os.path.join(settings.MEDIA_ROOT, image_path))
+            return video_path, image_path
+        elif self.provider_youtube_url:
+            with YoutubeDL({'format': 'bestvideo/best'}) as ydl:
+                result = ydl.extract_info(
+                    self.provider_youtube_url,
+                    download=False  # We just want to extract the info
+                )
+            video = result['entries'][0] if 'entries' in result else result  # Can be a playlist or a list of videos
+            stream_url = video['url']
+            utils.video_and_image_from_m3u8(stream_url, self.video_seconds, os.path.join(settings.MEDIA_ROOT, video_path), os.path.join(settings.MEDIA_ROOT, image_path))
+            return video_path, image_path
