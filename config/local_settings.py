1ac3e89e6769a895f2ee6feabff1075a3c889df7
antoni-oliver <46537504+antoni-oliver@users.noreply.github.com>
2026-02-12 15:15:09 +0100

    Refactor settings to remove production configuration
    Removed production settings and added LOCAL_SETTINGS to development settings.

--- config/local_settings.py
+++ config/local_settings.py
@@ -1,13 +1,10 @@
 # Production settings
-class PRODUCTION_SETTINGS:
-    DEBUG = False
-    ALLOWED_HOSTS = ['.localhost', 'ocupacioplatges.uib.es']
-
 
 # Development settings
 class DEVELOPMENT_SETTINGS:
     DEBUG = True
     ALLOWED_HOSTS = ['*']
+    LOCAL_SETTINGS = "j3guqzk3kll97@vx+qaid7es74$t)^mq6j!-)fcqc-9+e$q7js"
 
 
 LOCAL_SETTINGS = DEVELOPMENT_SETTINGS
