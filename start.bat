d5f0456eaf8790da1c792193f6ad03cf030b9a59
Guilherme Mossi Bento <guilhermemossibento@gmail.com>
2026-03-23 10:41:55 +0100

    chore(start.bat): add command to start the scheduler alongside the server for improved functionality

--- start.bat
+++ start.bat
@@ -1,5 +1,4 @@
 call .venv\Scripts\Activate.bat
+start python manage.py run_scheduler
 rem uvicorn main:app --host 0.0.0.0 --port 8001
-python .\manage.py crontab add
-python .\manage.py crontab add
 python .\manage.py runserver 8001
