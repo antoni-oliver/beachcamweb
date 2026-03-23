93dc12181e4f198046dcc9b5e798113bc72defd3
Guilherme Mossi Bento <guilhermemossibento@gmail.com>
2026-03-23 09:20:58 +0100

    feat(models.py): add validation to ensure predicted_image is set when predicted_crowd_count is provided to maintain data integrity fix(models.py): allow blank values for predicted_crowd_count field to improve flexibility in data entry

--- apps/prediction/models.py
+++ apps/prediction/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from apps.webcam.models import WebCam
 
@@ -18,8 +19,16 @@ class Snapshot(models.Model):
     mask_sand_and_water = models.ImageField(upload_to='masks/sand_and_water/', blank=True, null=True, help_text="Mask of the whole area of interest (beach + swimming water). For non-movable webcams only.")
 
     # Prediction data
-    predicted_crowd_count = models.FloatField(null=True)
+    predicted_crowd_count = models.FloatField(null=True, blank=True)
     predicted_image = models.ImageField(null=True, blank=True, upload_to='img/predictions/')
+
+    def clean(self):
+        if self.predicted_crowd_count is not None and not self.predicted_image:
+            raise ValidationError({'predicted_image': 'Predicted image is required when predicted crowd count is set.'})
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Snapshot {self.webcam.beach.beach_name} - {self.ts}'
