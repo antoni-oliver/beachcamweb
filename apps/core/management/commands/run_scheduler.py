cec38c9aeff656034ef9422d8120af48b8f98b06
Guilherme Mossi Bento <guilhermemossibento@gmail.com>
2026-03-23 10:33:57 +0100

    fix(run_scheduler.py): change CronTrigger from day to month for run_pipeline job to ensure it runs annually in January

--- apps/core/management/commands/run_scheduler.py
+++ apps/core/management/commands/run_scheduler.py
@@ -9,5 +9,5 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         scheduler = BlockingScheduler()
         scheduler.add_job(download_and_process.main, CronTrigger(minute='*/30'), next_run_time=datetime.now())
-        scheduler.add_job(lambda: call_command('run_pipeline'), CronTrigger(day=1, hour=2, minute=0))
+        scheduler.add_job(lambda: call_command('run_pipeline'), CronTrigger(month=1, hour=2, minute=0))
         scheduler.start()
