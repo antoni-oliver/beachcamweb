204d3c634613c9b2b2e476b59058f903f5d413dd
Guilherme Mossi Bento <guilhermemossibento@gmail.com>
2026-03-25 10:09:20 +0100

    fix(run_scheduler.py): update job scheduling times for download_and_process and run_pipeline to ensure correct execution timing

--- apps/core/management/commands/run_scheduler.py
+++ apps/core/management/commands/run_scheduler.py
@@ -8,6 +8,6 @@ import download_and_process
 class Command(BaseCommand):
     def handle(self, *args, **options):
         scheduler = BlockingScheduler()
-        scheduler.add_job(download_and_process.main, CronTrigger(hour=1), next_run_time=datetime.now())
-        scheduler.add_job(lambda: call_command('run_pipeline'), CronTrigger(month=1, hour=2, minute=0))
+        scheduler.add_job(download_and_process.main, CronTrigger(minute=0), next_run_time=datetime.now())
+        scheduler.add_job(lambda: call_command('run_pipeline'), CronTrigger(month=1, hour=1, minute=0))
         scheduler.start()
